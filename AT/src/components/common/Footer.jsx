e05d59138f4086dc01dbf21f020efcd99496235c
harim061 <doris5093@duksung.ac.kr>
2023-09-14 00:36:30 +0900

    Refactor [#2] main refactor
    메인 코드 AboutP 수정
    
    Fixes: #2

--- AT/src/components/common/Footer.jsx
+++ AT/src/components/common/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         className="waves"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28"
+        viewBox="0 24 150 28" 
         preserveAspectRatio="none"
         shapeRendering="auto">
         <defs>
